e7a0d1692af6e8feb224f461855b728b53d6ff17
Aras <recepkoseoglu@hotmail.com>
2019-01-05 14:49:17 +0300

    port change to 3001

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ PACKAGE_VERSION=$(cat ./package.json \
 docker container kill sample-vue
 
 docker build --rm -f "Dockerfile" -t arasksgl/sample-vue:$PACKAGE_VERSION .
-docker run --rm -d -p 9000:9000/tcp --name=sample-vue arasksgl/sample-vue:$PACKAGE_VERSION
+docker run --rm -d -p 3001:3001/tcp --name=sample-vue arasksgl/sample-vue:$PACKAGE_VERSION
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 3001
 
 app.set('port', port)
 
